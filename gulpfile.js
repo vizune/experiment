abf23ffd7f4ae1f5ea0abf3780af34908d21eae9
Sasha Richardson <sasha@thetin.net>
2017-08-09 17:54:17 +0100

    Gulp installed

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// Include gulp 
+var gulp = require('gulp');
+
+// Include plugins
+var jshint = require('gulp-jshint'),
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	cleancss = require('gulp-clean-css'),
+	rename = require('gulp-rename');
+	
+// Lint - checks JS files for errors in the code
+gulp.task('lint', function() {
+	return gulp.src('src/js/*js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+// Sass - Compiles sass files from src/scss and saves the compiled css files into dist/css
+gulp.task('sass', function() {
+	return gulp.src('src/scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('cssbundle', function() {
+	return gulp.src('src/scss/*.scss')
+		.pipe(sass())
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('dist/css'))
+		.pipe(rename('style.min.css'))
+		.pipe(cleancss())
+		.pipe(gulp.dest('dist/css'));
+});
+
+// Scripts - Concatenates all js files in src/js, saves the output to dist/js then also creates a minified / renamed version of the concatenated file in dist/js
+gulp.task('jsbundle', function() {
+	return gulp.src([
+		'!src/js/vendor/*.js',
+		'src/js/**/*.js'
+		])
+		.pipe(concat('bundle.js'))
+		.pipe(gulp.dest('dist/js'))
+		.pipe(rename('bundle.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/js'));
+});
+
+// JavaScript libraries copied across to dist folder
+gulp.task('library', function() {
+	return gulp.src('src/js/vendor/*.js')
+		.pipe(gulp.dest('dist/js/vendor'));
+});
+
+// Watch - method will listen for changes made to files and automatically run tasks
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Default task - used as a group reference to our other tasks. This will be the task that is run upon entering gulp in the command line
+gulp.task('default', ['lint', 'cssbundle', 'jsbundle', 'library', 'watch']);
