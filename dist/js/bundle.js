e5f9862221dd52f76dbeef9251d2ce356d824d53
Sasha Richardson <sasha@thetin.net>
2017-08-10 17:01:51 +0100

    Animation

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,19 +1,19 @@
 var left = anime({
 	targets: '#art .left',
 	translateX: -250,
-	easing: 'easeInOutQuart',
+	//easing: 'easeInOutQuart',
 	direction: 'alternate',
 	loop: true,
 	delay: function(el, i, l) { return i * 100; },
-	duration: 1000
+	elasticity: function(el, i, l) { return (i * 50); },
 });
 
 var right = anime({
 	targets: '#art .right',
 	translateX: 250,
-	easing: 'easeInOutQuart',
+	//easing: 'easeInOutQuart',
 	direction: 'alternate',
 	loop: true,
 	delay: function(el, i, l) { return i * 100; },
-	duration: 1000
+	elasticity: function(el, i, l) { return (i * 50); },
 });
